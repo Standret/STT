c5877523afce23530d938276aac2dc61613a19ad
Standret <pstandret@gmail.com>
2019-03-12 15:25:31 +0200

    init commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0'
+use_frameworks!
+
+target 'STT' do
+  pod 'RxSwift', '~> 4.0'
+end
+
+
---
+++ STT/STT.h
@@ -0,0 +1,19 @@
+//
+//  STT.h
+//  STT
+//
+//  Created by Peter Standret on 3/12/19.
+//  Copyright © 2019 standret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STT.
+FOUNDATION_EXPORT double STTVersionNumber;
+
+//! Project version string for STT.
+FOUNDATION_EXPORT const unsigned char STTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STT/PublicHeader.h>
+
+
