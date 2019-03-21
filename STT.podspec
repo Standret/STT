80b27ce9eb851771c6191fd86172457fb7ef0a55
Standret <pstandret@gmail.com>
2019-03-21 16:56:41 +0200

    fixed no interntet connection error

--- STT.podspec
+++ STT.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "STT"
-  s.version      = "1.1.2"
+  s.version      = "1.1.3"
   s.summary      = "Added new bindings. Several fixes in existing code"
 
   s.description  = <<-DESC
--- STT/Components/Core/SttBaseError.swift
+++ STT/Components/Core/SttBaseError.swift
@@ -44,7 +44,7 @@ public enum SttBaseError: Error, SttBaseErrorType {
             result = (tempRes.1, "Api: \(tempRes.0) \(tempRes.1)")
         case .connectionError(let concreate):
             let tempRes = concreate.getMessage()
-            result = ("Connection: \(tempRes.0)", tempRes.1)
+            result = ("\(tempRes.0)", tempRes.1)
         case .jsonConvert(let message):
             result = ("Json convert", message)
         case .unkown(let message):
