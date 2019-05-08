0e10171793a7f7835f22c45e437d23f75d0e30ff
Standret <pstandret@gmail.com>
2019-05-08 16:53:15 +0300

    updated collection view source
    added missing method
    added scrollHandler

---
+++ STT/Bindings/SttBaseCollectionViewSource.swift
@@ -0,0 +1,232 @@
+//
+//  SttBaseCollectionViewSource.swift
+//  STT
+//
+//  Created by Peter Standret on 5/8/19.
+//  Copyright © 2019 Peter Standret <pstandret@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+import UIKit
+
+open class SttBaseCollectionViewSource<TPresenter: SttViewInjector>: NSObject, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private(set) var _collectionView: UICollectionView
+    
+    private(set) public var cellIdentifier: [String]
+    private(set) public var sectionIdentifier: [String]
+    
+    public var container = [SttTypeActionCollectionView: [SttScrollViewHandlerType]]()
+    
+    public init(collectionView: UICollectionView, cellIdentifiers: [SttIdentifiers], sectionIdentifier: [String]) {
+        
+        _collectionView = collectionView
+        
+        self.cellIdentifier = cellIdentifiers.map({ $0.identifers })
+        self.sectionIdentifier = sectionIdentifier
+        
+        for item in cellIdentifiers {
+            if !item.isRegistered {
+                collectionView.register(UINib(nibName: item.nibName ?? item.identifers, bundle: nil), forCellWithReuseIdentifier: item.identifers)
+            }
+        }
+        
+        for item in sectionIdentifier {
+            collectionView.register(UINib(nibName: item, bundle: nil),
+                                    forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                    withReuseIdentifier: item)
+        }
+        
+        super.init()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    open func collectionViewReusableCell(at indexPath: IndexPath) -> String {
+        return cellIdentifier.first!
+    }
+    open func collectionViewReusableSection(at indexPath: IndexPath) -> String {
+        return sectionIdentifier.first!
+    }
+    
+    open func presenter(at indexPath: IndexPath) -> TPresenter {
+        fatalError("Should be implemented")
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    open func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    open func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return UICollectionReusableView()
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    open func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) { }
+    
+    open func indexTitles(for collectionView: UICollectionView) -> [String]? {
+        return []
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, indexPathForIndexTitle title: String, at index: Int) -> IndexPath {
+        return IndexPath()
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .zero
+    }
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return .zero
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    open func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.collectionView(collectionView, didSelectItemAt: indexPath, with: presenter(at: indexPath))
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        self.collectionView(collectionView, didDeselectItemAt: indexPath, with: presenter(at: indexPath))
+    }
+    
+    /// Tells the delegate that the item at the specified index path was selected.
+    /// - Parameter presenter: represent selected cell's presrnter
+    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath, with presenter: TPresenter) { }
+    
+    /// Tells the delegate that the item at the specified index path was deselected.
+    /// - Parameter presenter: represent selected cell's presrnter
+    open func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath, with presenter: TPresenter) { }
+    
+    // MARK: - HIGLIGHT
+    
+    open func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        self.collectionView(collectionView, didHighlightItemAt: indexPath, with: presenter(at: indexPath))
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        self.collectionView(collectionView, didUnhighlightItemAt: indexPath, with: presenter(at: indexPath))
+    }
+    
+    /// Tells the delegate that the item at the specified index path was highlighted.
+    /// - Parameter presenter: represent selected cell's presrnter
+    open func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath, with presenter: TPresenter) { }
+    
+    /// Tells the delegate that the item at the specified index path was unhighlighted.
+    /// - Parameter presenter: represent selected cell's presrnter
+    open func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath, with presenter: TPresenter) { }
+    
+    
+    open func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) { }
+    open func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) { }
+    
+    open func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) { }
+    open func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) { }
+    
+    open func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+    open func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) { }
+    
+    open func collectionView(_ collectionView: UICollectionView, transitionLayoutForOldLayout fromLayout: UICollectionViewLayout, newLayout toLayout: UICollectionViewLayout) -> UICollectionViewTransitionLayout {
+        return UICollectionViewTransitionLayout(currentLayout: fromLayout, nextLayout: toLayout)
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, canFocusItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    open func collectionView(_ collectionView: UICollectionView, shouldUpdateFocusIn context: UICollectionViewFocusUpdateContext) -> Bool {
+        return true
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) { }
+    
+    open func indexPathForPreferredFocusedView(in collectionView: UICollectionView) -> IndexPath? {
+        return nil
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, targetIndexPathForMoveFromItemAt originalIndexPath: IndexPath, toProposedIndexPath proposedIndexPath: IndexPath) -> IndexPath {
+        return proposedIndexPath
+    }
+    open func collectionView(_ collectionView: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
+        return proposedContentOffset
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, shouldSpringLoadItemAt indexPath: IndexPath, with context: UISpringLoadedInteractionContext) -> Bool {
+        return true
+    }
+    
+    open func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.container[.scrollViewDidScroll]?.forEach({ $0.handle(scrollView) })
+    }
+    
+    open func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) { }
+}
+
+extension SttBaseCollectionViewSource {
+    
+    /// Add end scrolled handler
+    public func addEndScrollHandler<T: UIViewController>(delegate: T, callback: @escaping (T) -> Void) {
+        self.container[.scrollViewDidScroll] = self.container[.scrollViewDidScroll] ?? [SttScrollViewHandlerType]()
+        self.container[.scrollViewDidScroll]!.append(SttEndScrollHandler.init(delegate: delegate, { (delegate, _) in callback(delegate) } ))
+    }
+}
---
+++ STT/Bindings/SttHandlerCollectionView.swift
@@ -0,0 +1,68 @@
+//
+//  SttHandlerCollectionView.swift
+//  STT
+//
+//  Created by Peter Standret on 5/8/19.
+//  Copyright © 2019 Peter Standret <pstandret@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+import UIKit
+
+public enum SttTypeActionCollectionView {
+    case scrollViewDidScroll
+    case scrollViewDidEndDecelerating
+}
+
+public protocol SttScrollViewHandlerType {
+    func handle(_ scrollView: UIScrollView)
+}
+
+open class SttEndScrollHandler<Target: UIScrollView>: SttScrollViewHandlerType {
+    
+    private var handlers = [SttDelegatedCall<Target>]()
+    
+    open var callBackEndPixel: Int = 150
+    
+    public init() { }
+    public init<T: AnyObject>(delegate: T, _ handler: @escaping (T, Target) -> Void) {
+        handlers.append(SttDelegatedCall(to: delegate, with: handler))
+    }
+    
+    private var inPosition: Bool = false
+    open func handle(_ scrollView: UIScrollView) {
+        
+        let x = scrollView.contentOffset.y
+        let width = scrollView.contentSize.height - scrollView.bounds.height - CGFloat(callBackEndPixel)
+        
+        if (scrollView.contentSize.height > scrollView.bounds.height) {
+            if (x > width) {
+                if (!inPosition) {
+                    handlers.forEach({ $0.callback(scrollView as! Target) })
+                }
+                inPosition = true
+            }
+            else {
+                inPosition = false
+            }
+        }
+    }
+}
