af37b1bfa4b7a2efa842111b30f870cd54e5977e
Standret <pstandret@gmail.com>
2019-06-04 14:02:30 +0300

    added listener for scroll view and table view

--- STT/Bindings/SttListenerScrollView.swift
+++ STT/Bindings/SttListenerScrollView.swift
@@ -3,7 +3,75 @@
 //  STT
 //
 //  Created by Peter Standret on 6/4/19.
-//  Copyright © 2019 standret. All rights reserved.
+//  Copyright © 2019 Peter Standret <pstandret@gmail.com>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import Foundation
+import UIKit
+
+enum TypeActionScrollView {
+    case contentOffsetChanged
+    case contentSizeChanged
+}
+
+class SttListenerScrollView: UIScrollView {
+    
+    private var handlers = [TypeActionScrollView: [() -> Void]]()
+    
+    func addTarget(type: TypeActionScrollView, handler: @escaping () -> Void) {
+        handlers[type] = handlers[type] ?? [() -> Void]()
+        handlers[type]?.append(handler)
+    }
+    
+    override var contentOffset: CGPoint {
+        didSet {
+            handlers[.contentOffsetChanged]?.forEach({ $0() })
+        }
+    }
+    
+    override var contentSize: CGSize {
+        didSet {
+            handlers[.contentSizeChanged]?.forEach({ $0() })
+        }
+    }
+}
+
+class SttListenerTableView: UITableView {
+    
+    private var handlers = [TypeActionScrollView: [() -> Void]]()
+    
+    func addTarget(type: TypeActionScrollView, handler: @escaping () -> Void) {
+        handlers[type] = handlers[type] ?? [() -> Void]()
+        handlers[type]?.append(handler)
+    }
+    
+    override var contentOffset: CGPoint {
+        didSet {
+            handlers[.contentOffsetChanged]?.forEach({ $0() })
+        }
+    }
+    
+    override var contentSize: CGSize {
+        didSet {
+            handlers[.contentSizeChanged]?.forEach({ $0() })
+        }
+    }
+}
